836702ee0dae92c85ff3f8a1a60bdc43e531ee0c
Anupama Gopewar <anupamagopewar@gmail.com>
2026-09-18 18:20:05 +0530

    fix: use psycopg database driver

--- backend/database.py
+++ backend/database.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL").replace("postgresql://", "postgresql+psycopg://")
 
 engine = create_engine(DATABASE_URL)
 
